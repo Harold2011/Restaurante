ac3f0bb38c406a3d39542ad780efca53136f9068
Harold Mairn <32544291+Harold2011@users.noreply.github.com>
2025-04-29 16:23:51 -0500

    se agrego un h1

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php include 'base.php'; ?>
 <?php include 'nav.php'; ?>
 <main>
+    <h1>Esto es un cambio</h1>
     <h2>Contenido Principal</h2>
     <p>Este es el contenido principal de la página.</p>
 </main>
